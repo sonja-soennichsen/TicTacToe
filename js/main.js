ca15676b7597a8e6242e9d4208450e272850821b
Sonja Soennichsen <sonja.soennichsen@axelspringer.com>
2021-03-23 11:59:28 +0100

    Basic Structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log("Hello")
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let board;
+let turn = "X";
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
+
+/*----- functions -----*/
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+        return square === event.target;
+    });
+    [idx] = turn;
+    console.log(board);
+};
+
+function init() {
+    board = [
+        '', '', '',
+        '', '', '',
+        '', '', '',
+    ];
+    render();
+};
+
+function render() {
+    board.forEach(function(mark, index) {
+        squares[index].textContent = mark;
+    });
+};
+
+init();
