97b96c4c814c1b855be0aa5b9ef57a06981ac23b
Sonja Soennichsen <sonja.soennichsen@axelspringer.com>
2021-03-23 12:32:13 +0100

    Adding Functionality

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 /*----- app's state (variables) -----*/
 let board;
 let turn = "X";
+let win;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector("h2");
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('reset-button').addEventListener('click',
+    init);
 
 /*----- functions -----*/
-function handleTurn(event) {
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] &&
+            board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
+    return winner ? winner : board.includes('') ? null : 'T';
+};
+
+function handleTurn() {
     let idx = squares.findIndex(function(square) {
         return square === event.target;
     });
-    [idx] = turn;
-    console.log(board);
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render();
 };
 
 function init() {
@@ -32,6 +57,7 @@ function render() {
     board.forEach(function(mark, index) {
         squares[index].textContent = mark;
     });
+    messages.textContent = win === 'T' ? `It's a Tie!` : win ? `${win} wins the game` : `It's ${turn}'s turn!`;
 };
 
 init();
